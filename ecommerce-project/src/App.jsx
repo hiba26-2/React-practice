191cfaef08908c9392a1b05fb03b15df39b70b31
Hiba <hibaabdulhamid41@gmail.com>
2026-09-10 14:22:08 +0300

    Move the orders page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { OrdersPage } from './pages/OrdersPage'
 import {HomePage} from './pages/HomePage'
 import {Routes,Route} from 'react-router'
 import {CheckoutPage} from './pages/CheckoutPage'
@@ -11,7 +11,10 @@ function App() {
     
     <Routes>
       <Route  index  element ={<HomePage/>}/>
+     
       <Route  path ="checkout"  element ={<CheckoutPage/>}/>
+       <Route  path ="orders"  element ={<OrdersPage/>}/>
+        <Route  path ="tracking"  element ={<CheckoutPage/>}/>
    </Routes>
   
   )
