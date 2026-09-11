561239d7b67c7e3c18383853e4fb4ca2cd75e1b5
Hiba <hibaabdulhamid41@gmail.com>
2026-09-11 15:26:08 +0300

    Add icons to tracking page

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -5,7 +5,7 @@ export function TrackingPage(){
     return(
         <>
           <title>Tracking</title>
-          
+          <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
  <Header/>
 
     <div className="tracking-page">
