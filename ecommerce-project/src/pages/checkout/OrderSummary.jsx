95c2f3be83b250dc6337830931c4da4b5458b878
Hiba <hibaabdulhamid41@gmail.com>
2026-09-14 17:26:43 +0300

    Separated Cart Item details

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,10 +1,8 @@
  import dayjs from "dayjs";
- import { formatMoney } from "../../utils/money";
  import { DeliveryOptions } from "./DeliveyOptions";
+import { CartItemDetails } from "./cartItemDetails";
  export function OrderSummary({cart,deliveryOptions}){
-return(
-
-
+   return(
          <div className="order-summary">
 
           {deliveryOptions.length >0 && cart.map((cartItem)=>{
@@ -20,29 +18,7 @@ return(
             </div>
 
             <div className="cart-item-details-grid">
-              <img className="product-image"
-                src={cartItem.product.image} />
-
-              <div className="cart-item-details">
-                <div className="product-name">
-                  {cartItem.product.name}
-                </div>
-                <div className="product-price">
-                  {formatMoney(cartItem.product.pricecent)}
-                </div>
-                <div className="product-quantity">
-                  <span>
-                    Quantity: <span className="quantity-label">2</span>
-                  </span>
-                  <span className="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span className="delete-quantity-link link-primary">
-                    Delete
-                  </span>
-                </div>
-              </div>
-
+              <CartItemDetails cartItem={cartItem} />
              <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
             </div>
           </div>
