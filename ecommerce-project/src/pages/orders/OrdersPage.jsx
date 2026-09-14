f981ce1d3f66bbe0748ad323ead662c9f26616a1
Hiba <hibaabdulhamid41@gmail.com>
2026-09-14 18:08:00 +0300

    Separate Order details and Order Header from Orders Page

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {useState,useEffect,Fragment} from 'react'
 import {Header} from '../../components/Header'
+import { OrdersGrid } from './OrdersGrid'
 import './OrdersPage.css'
 
 export function OrdersPage({cart}){
