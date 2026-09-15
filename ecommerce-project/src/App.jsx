dc275cad1bf9bb966a85ae40ebea70f8a4bbd78e
Hiba <hibaabdulhamid41@gmail.com>
2026-09-15 03:02:06 +0300

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -10,24 +10,23 @@ import {CheckoutPage} from './pages/checkout/CheckoutPage'
 import './App.css'
 
 function App() {
-
+      const loadCart= async()=>{
+        const response= await axios.get('/api/cart-items?expand=product')
+       setCart (response.data)
+       
+      };
     const [cart,setCart]=useState([])
     useEffect(()=>{
-      const fetchAppData= async()=>{
-       let response= await axios.get('/api/cart-items?expand=product')
-       
-          setCart (response.data)
-       
-      }
-     fetchAppData(); 
+    
+     loadCart(); 
  
     },[])
   return (
     
     <Routes>
-      <Route  index  element ={<HomePage cart={cart}/>}/>
+      <Route  index  element ={<HomePage cart={cart} loadCart={loadCart}/>}/>
      
-        <Route  path ="checkout"  element ={<CheckoutPage cart={cart}/>}/>
+        <Route  path ="checkout"  element ={<CheckoutPage cart={cart} />}/>
         <Route  path ="orders"  element ={<OrdersPage cart ={cart}/>}/>
         <Route  path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />}/>
         <Route path ="*" element={<PageNotFound/>}/>
