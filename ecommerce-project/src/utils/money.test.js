f8542297b3cc0b8f45225bfb5605edd1567aed4c
Hiba <hibaabdulhamid41@gmail.com>
2026-09-18 23:28:18 +0300

    Create unit test for formatMoney

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,17 @@
+import {it,expect,describe} from 'vitest';
+import {formatMoney} from './money';
+
+describe('formatMoney',()=>{
+it('format 1999 cents as $19.99',()=>{
+expect(formatMoney(1999)).toBe('$19.99')
+});
+//name of test, function- use expect to check if result is correct
+
+it('displays 2 decimals', ()=>{
+    expect(formatMoney(1090)).toBe('$10.90');
+    expect(formatMoney(100)).toBe('$1.00')
+    //both have to pass to pass the test
+});
+})
+
+// to group tests use describe, a test group is called test suite
