fbb9015ba745f711cae735d780469b631258cfb5
Hiba <hibaabdulhamid41@gmail.com>
2026-09-19 09:55:48 +0300

    Use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,65 +1,69 @@
-import {it,expect,describe,vi} from 'vitest';
-import {render,screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {Product} from './Product';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import axios from 'axios';
-vi.mock('axios')
- //vi, to create mock(fake function)
-describe('Product component',()=>{
- it('displays product details correctly',()=>{
-    const product={
-    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-8-pack.jpg",
-    name: "2-Ply Kitchen Paper Towels - 8 Pack",
-    rating: {
-      stars: 4.5,
-      count: 1045
-    },
-    priceCents: 1899,
-    keywords: ["kitchen", "kitchen towels", "tissues"]
-  }
-  const loadCart=vi.fn();
-    render(<Product product={product} loadCart={loadCart} />)
- 
-    expect(screen.getByText('2-Ply Kitchen Paper Towels - 8 Pack')
-    ).toBeInTheDocument();
+import { Product } from './Product';
+
+vi.mock('axios');
+
+describe('Product component', () => {
+  let product;
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"]
+    };
+
+    loadCart = vi.fn();
+  });
+
+  it('displays the product details correctly', () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
     expect(
-    screen.getByText('$18.99')
-  ).toBeInTheDocument();
-   expect(
-    screen.getByTestId('product-image')
-   ).toHaveAttribute('src','images/products/kitchen-paper-towels-8-pack.jpg')
-   expect(screen.getByTestId('product-rating-stars-image')
-  ).toHaveAttribute('src','images/ratings/rating-45.png')
-  expect(
-    screen.getByText('1045')
-  ).toBeInTheDocument();
- });
+      screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
+    ).toBeInTheDocument();
 
- it('adds a product to the cart',async()=>{
-      const product={
-    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-8-pack.jpg",
-    name: "2-Ply Kitchen Paper Towels - 8 Pack",
-    rating: {
-      stars: 4.5,
-      count: 1045
-    },
-    priceCents: 1899,
-    keywords: ["kitchen", "kitchen towels", "tissues"]
-  }
-  const loadCart=vi.fn();
-    render(<Product product={product} loadCart={loadCart} />)
-    const user=userEvent.setup();
-    const addToCartButton =screen.getByTestId('add-to-cart-button')
-   await user.click(addToCartButton)
+    expect(
+      screen.getByText('$10.90')
+    ).toBeInTheDocument();
 
-   expect(axios.post).toHaveBeenCalledWith('/api/cart-items',
-    {
-      productId:'aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f',
-      quantity:1
-    }
-   );
-   expect(loadCart).toHaveBeenCalled()
- })
+    expect(
+      screen.getByTestId('product-image')
+    ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
+
+    expect(
+      screen.getByTestId('product-rating-stars-image')
+    ).toHaveAttribute('src', 'images/ratings/rating-45.png');
+
+    expect(
+      screen.getByText('87')
+    ).toBeInTheDocument();
+  });
+
+  it('adds a product to the cart', async () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId('add-to-cart-button');
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1
+      }
+    );
+    expect(loadCart).toHaveBeenCalled();
+  });
 });
