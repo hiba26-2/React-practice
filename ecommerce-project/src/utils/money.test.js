23979a34aa49975ba5e349012487ac8e4b7f5e6b
Hiba <hibaabdulhamid41@gmail.com>
2026-09-19 11:40:30 +0300

    Add a test for formatMoney with negative number

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -10,9 +10,17 @@ expect(formatMoney(1999)).toBe('$19.99')
 it('displays 2 decimals', ()=>{
     expect(formatMoney(1090)).toBe('$10.90');
     expect(formatMoney(100)).toBe('$1.00')
+    });
+it('works with the number 0',()=>{
     expect(formatMoney(0)).toBe('$0.00')
+    }) 
+it('works with negative numbers', () => {
+    expect(formatMoney(-999)).toBe('-$9.99');
+    expect(formatMoney(-100)).toBe('-$1.00');
+  });
+   
     //both have to pass to pass the test
-});
+
 })
 
 // to group tests use describe, a test group is called test suite
